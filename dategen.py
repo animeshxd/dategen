18574e75a431100b242362c0b7d6b16a85e08a00
Animeshxd <Animeshxd@outlook.com>
2021-07-15 16:00:20 +0530

    Full Async

--- dategen.py
+++ dategen.py
@@ -7,19 +7,20 @@ class DateGen:
     def __init__(self):
         pass
     @staticmethod
-    def today() -> str:
+    async def today() -> str:
         return datetime.now().strftime("%Y-%m-%d")
     @staticmethod
     async def days(Range: int):
         return [(datetime.now() -  timedelta(days=i)).strftime("%Y-%m-%d")  for i in range(Range)]
     @staticmethod
-    def last_week(last: int):
+    async def last_week(last: int):
         last_week = datetime.now() - timedelta(weeks=last)
         return date_range(start=last_week.strftime('%Y/%m/%d'), end=datetime.now().strftime('%Y/%m/%d'))
 
 if __name__ == '__main__':
     date = DateGen()
-    print(date.today())
+    print(asyncio.run(date.today()))
     print(asyncio.run(date.days(30)))    
-    print(date.today() in date.last_week(5))
+    # print(date.today() in date.last_week(5))
+    
 
