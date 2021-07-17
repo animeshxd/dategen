247991afdd88cde7526bbf2e036c7d0cf62a35d1
Animesh Murmu <Am2646374@gmail.com>
2021-07-17 15:26:18 +0530

    Added: this month generator

--- dategen.py
+++ dategen.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta 
 import asyncio
 from pandas import date_range
-
+from calendar import Calendar
 
 class DateGen:
     def __init__(self):
@@ -16,11 +16,19 @@ class DateGen:
     async def last_week(last: int):
         last_week = datetime.now() - timedelta(weeks=last)
         return date_range(start=last_week.strftime('%Y/%m/%d'), end=datetime.now().strftime('%Y/%m/%d'))
+    @staticmethod
+    async def this_month():
+        year = datetime.today().year
+        month = datetime.today().month
+        iter_ = Calendar().itermonthdates(year,month)
+        return [ i.strftime('%Y-%m-%d') for i in iter_ if i.month == month]
 
 if __name__ == '__main__':
     date = DateGen()
     print(asyncio.run(date.today()))
-    print(asyncio.run(date.days(30)))    
+    print(asyncio.run(date.days(30)))   
+    print(asyncio.run(date.last_week(5)))
     # print(date.today() in date.last_week(5))
+    print(asyncio.run(date.this_month()))
     
 
